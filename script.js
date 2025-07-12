2ea3d22518f962863678490ca745b07a5c233fb2
Sudheesh karippadath <sudheeshks132@gmail.com>
2025-07-12 20:04:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1145,4 +1145,48 @@
 // })
 
 
+// Advanced example for routing with params
+
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/', (req,res) => {
+//     res.send("hello")
+// })
+
+// app.get('/products/:id', (req,res) => {
+//     const productId = parseInt(req.params.id)
+//     const products = [
+//         {
+//             id:1,
+//             name:'product1',
+//             price:200
+//         },
+//         {
+//             id:2,
+//             name:'product2',
+//             price:300
+//         },
+//         {
+//             id:3,
+//             name:'product3',
+//             price:400
+//         },
+//     ]
+
+//     const getSingleProduct = products.find(product => product.id === productId);
+//     if(getSingleProduct){
+//         res.send(getSingleProduct)
+//     }else{
+//         res.status(404).send("No product found")
+//     }
+// })
+
+
+// app.listen(3000,() => {
+//     console.log("Server started....");
+// })
+
+
 
